02c18becec821e2a134a3a4937c63901e147e854
Bryan Andagoya <bryanandagoya@gmail.com>
2026-07-24 17:16:38 -0500

    Initial Commit

---
+++ LittleLemonIos/LittleLemonIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonIosApp.swift
+//  LittleLemonIos
+//
+//  Created by BRYAN FABIAN ANDAGOYA CURAY on 24/7/26.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
